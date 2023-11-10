12c628b239c5691dddcec222bbe9c693235096df
JohnQuimson <john.quimson777@gmail.com>
2023-11-10 16:26:29 +0100

    feat: add img to html w/ js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/*
+Quello che vogliamo andare a creare con JS è:
+<div class="item">
+  <img src="img/arrayImages[i]" alt="" />
+</div>
+
+*/
+
+const arrayImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const slider = document.querySelector('.items');
+//dichiaro quale slide[i] deve avere la class 'active'
+let slideAttivaIndex = 0;
+
+for (let i = 0; i < arrayImages.length; i++) {
+  const elementSlide = document.createElement('div');
+  elementSlide.classList.add('item');
+
+  if (i === slideAttivaIndex) {
+    elementSlide.classList.add('active');
+  }
+
+  const elementImg = document.createElement('img');
+  elementImg.src = `img/${arrayImages[i]}`;
+  elementImg.alt = `img ${arrayImages[i + 1]}`;
+
+  elementSlide.append(elementImg);
+  slider.append(elementSlide);
+}
