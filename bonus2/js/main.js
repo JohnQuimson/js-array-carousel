3d86762e880598715ce463a4512eb394cd98d976
JohnQuimson <john.quimson777@gmail.com>
2023-11-11 16:41:37 +0100

    feat: creazione raccolta thumbnails

--- bonus2/js/main.js
+++ bonus2/js/main.js
@@ -11,10 +11,17 @@ const arrayImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 const slider = document.querySelector('.items');
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+const container = document.querySelector('.container');
 //dichiaro quale slide[i] deve avere la class 'active'
 let slideAttivaIndex = 0;
 
-/* AGGIUNTA ELEMENTI ALL'HTML */
+/*
+-------------------------
+AGGIUNTA ELEMENTI ALL'HTML
+-------------------------
+ */
+
+//AGGIUNTA DIV CON IMG
 for (let i = 0; i < arrayImages.length; i++) {
   const elementSlide = document.createElement('div');
   elementSlide.classList.add('item');
@@ -27,31 +34,57 @@ for (let i = 0; i < arrayImages.length; i++) {
   elementImg.src = `img/${arrayImages[i]}`;
   elementImg.alt = `img ${arrayImages[i + 1]}`;
 
-  elementSlide.append(elementImg);
   slider.append(elementSlide);
+  elementSlide.append(elementImg);
 }
+//AGGIUNTA THUMBNAILS
+//creo un elemento div con classe raccolta-thumbnails dentro container
+const raccolta = document.createElement('div');
+raccolta.classList.add('raccolta-thumbnails');
 
-/* /AGGIUNTA ELEMENTI ALL'HTML */
+container.append(raccolta);
+
 //prendo in considerazione tutti gli elementi con class 'item', i quali vanno a finire in una specie di 'array'
 const domSlides = document.querySelectorAll('.item');
-console.log(domSlides);
+let lunghDomSlides = domSlides.length - 1;
 
-//Azioni al click del pulsante next
+/*
+-------------------------
+Azioni al click del pulsante next
+-------------------------
+ */
 next.addEventListener('click', function () {
-  console.log('Ho cliccato su Next');
-  if (slideAttivaIndex < domSlides.length - 1) {
-    domSlides[slideAttivaIndex].classList.remove('active');
+  //Di default rimuovo la classe active al primo elemento
+  domSlides[slideAttivaIndex].classList.remove('active');
+
+  //se il contatore del NodeList è minore del num di elementi dei NodeList, incrementa il contatore
+  if (slideAttivaIndex < lunghDomSlides) {
     slideAttivaIndex++;
-    domSlides[slideAttivaIndex].classList.add('active');
+
+    //se il contatore supera il limite, torna da capo
+  } else {
+    slideAttivaIndex = 0;
   }
+
+  domSlides[slideAttivaIndex].classList.add('active');
+  console.log('next');
+  console.log(`Immgine con 'active': domSlides[${slideAttivaIndex}]`);
 });
 
-//Azioni al click del pulsante preview
+/*
+-------------------------
+Azioni al click del pulsante prev
+-------------------------
+ */
 prev.addEventListener('click', function () {
-  console.log('Ho cliccato su Prev');
+  domSlides[slideAttivaIndex].classList.remove('active');
+
   if (slideAttivaIndex > 0) {
-    domSlides[slideAttivaIndex].classList.remove('active');
     slideAttivaIndex--;
-    domSlides[slideAttivaIndex].classList.add('active');
+  } else {
+    slideAttivaIndex = lunghDomSlides;
   }
+  domSlides[slideAttivaIndex].classList.add('active');
+  console.log('prev');
+  console.log(`Immgine con 'active': domSlides[${slideAttivaIndex}]`);
 });
